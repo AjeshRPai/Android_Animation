01e2896374f14a65cc73aae4bd6df8acc70a1be1
Ajesh Pai <ajesh.pai@capitalfloat.com>
2019-12-10 11:46:01 +0530

    Implementation of Wave Animation based on
    https://medium.com/s23nyc-tech/geometric-android-animations-using-the-canvas-dd687c43f3f4

--- app/src/main/java/com/example/ajeshpai/androidanimatios/IndexActivity.kt
+++ app/src/main/java/com/example/ajeshpai/androidanimatios/IndexActivity.kt
@@ -3,6 +3,7 @@ package com.example.ajeshpai.androidanimatios
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ajeshpai.androidanimatios.PhysicsBasedAnimations.IndexPhysicsBasedActivity
+import com.example.ajeshpai.androidanimatios.WaveAnimation.WaveAnimationActivity
 import com.example.ajeshpai.androidanimatios.transitions.IndexTransitionActivity
 import kotlinx.android.synthetic.main.activity_index2.*
 import org.jetbrains.anko.startActivity
@@ -12,6 +13,8 @@ class IndexActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index2)
+
+        button6.setOnClickListener { startActivity<WaveAnimationActivity>()}
 
 
         button4.setOnClickListener { startActivity<IndexPhysicsBasedActivity>() }
---
+++ app/src/main/java/com/example/ajeshpai/androidanimatios/WaveAnimation/WaveAnimationActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ajeshpai.androidanimatios.WaveAnimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ajeshpai.androidanimatios.R
+
+/**
+ * Implementation is based on
+ * https://medium.com/s23nyc-tech/geometric-android-animations-using-the-canvas-dd687c43f3f4
+ * */
+
+class WaveAnimationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wave_animation)
+    }
+}
