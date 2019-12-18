957bcf4b961454ae2045aed71cb3937941ef028d
Ajesh Pai <ajesh.pai@capitalfloat.com>
2019-12-18 12:04:55 +0530

    Implementation started of Path Animation based on
    https://medium.com/androiddevelopers/playing-with-paths-3fbc679a6f77?

---
+++ app/src/main/java/com/example/ajeshpai/androidanimatios/PathAnimations/PolygonPathAnimation.kt
@@ -0,0 +1,13 @@
+package com.example.ajeshpai.androidanimatios.PathAnimations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ajeshpai.androidanimatios.R
+
+class PolygonPath : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_polygon_path)
+    }
+}
